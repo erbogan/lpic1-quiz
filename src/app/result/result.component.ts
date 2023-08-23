21144d6f4f70786d87fcff67a9c715736bba0975
erbogan <eyup.erbogan@gmail.com>
2023-08-23 09:12:24 +0200

    "23.08.23 ilk"

--- src/app/result/result.component.ts
+++ src/app/result/result.component.ts
@@ -7,15 +7,13 @@ import { SharedService } from '../shared.service';
   styleUrls: ['./result.component.css']
 })
 export class ResultComponent {
-  // @Input() correctAnswer: number=0;
-  // @Input() falseAnswer: number=0;
-  // @Input() Der_Fragen_Zahl: number=0;
+
 
   constructor(private changeDetector: ChangeDetectorRef,
     public shs: SharedService) {}
 
   ngOnChanges() {
     this.changeDetector.detectChanges();
-    // this.correctAnswer = this.shs.getCorrectAnswer()
+
   }
 }
